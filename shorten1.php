17912dc2048d40272a998e2748b3a2e1ffbf9444
alsolaim94 <f.alsolaim@gmail.com>
2019-11-12 11:00:42 +0300

    Shortening URLs Project

---
+++ shorten1.php
@@ -0,0 +1,38 @@
+<?php
+
+session_start();
+
+// Include database configuration file
+require_once 'dbConfig.php';
+
+// Include URL Shortener library file
+require_once 'Shortener.class.php';
+
+// Initialize Shortener class and pass PDO object
+$shortener = new Shortener($db);
+
+
+// Long URL
+$longURL = $_POST['url'];
+$description = $_POST['description'];
+
+
+// Prefix of the short URL 
+$shortURL_Prefix = 'localhost/shortening/'; // with URL rewrite
+//$shortURL_Prefix = 'https://xyz.com/?c='; // without URL rewrite
+
+try{
+    // Get short code of the URL
+    $shortCode = $shortener->urlToShortCode($longURL,$description);
+    
+    // Create short URL
+    $shortURL = $shortURL_Prefix.$shortCode;
+    
+    // Display short URL
+//    $newurl = 'Short URL: '.$shortURL;
+    $_SESSION['newurl'] = $shortURL;
+        header("Location: result.php");
+}catch(Exception $e){
+    // Display error
+    echo $e->getMessage();
+}
